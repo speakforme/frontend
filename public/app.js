263f3bf99e301b86d6503498d5033b77f8176952
Nemo <me@captnemo.in>
2017-12-09 02:36:00 +0530

    Move methods to app.js

--- public/app.js
+++ public/app.js
@@ -4,6 +4,43 @@ var services = window.services;
 // This is the method that generates a response
 var makeResponse = function() {};
 
+// TODO: convert following methods to vue
+var emailContent = function(address) {
+  var content = document.getElementById('draft-notice-dept').innerText;
+  return address + '\n\n' + content;
+};
+
+var IEMobile = /IEMobile/i.test(navigator.userAgent);
+var isMobile =
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|Opera Mini/i.test(
+    navigator.userAgent
+  ) && !IEMobile;
+
+var sendEmailMobile = function(toAddress, subject, bccAddress, encodedBody) {
+  window.location.href =
+    'mailto:' +
+    encodeURIComponent(toAddress) +
+    '?subject=' +
+    encodeURIComponent(subject) +
+    '&bcc=' +
+    encodeURIComponent(bccAddress) +
+    '&body=' +
+    encodedBody;
+  window.location.hash = '#share';
+};
+
+var sendEmail = function() {
+  // var to, subject, bcc, body
+
+  var encodedBody = encodeURIComponent(body);
+
+  if (isMobile) {
+    sendEmailMobile(to, subject, bcc, encodedBody);
+    return false;
+  } else {
+  }
+};
+
 var app = new Vue({
   delimiters: ['<%', '%>'],
   el: '#resist-form',
@@ -25,14 +62,22 @@ var app = new Vue({
       return this.banks[this.bankIndex].name;
     },
     email: function() {
+      var e;
       switch (this.service.name) {
         case 'Bank':
-          return banks[this.bankIndex].email;
+          e = banks[this.bankIndex].email;
           break;
         default:
-          return services[this.serviceIndex].email;
+          e = services[this.serviceIndex].email;
           break;
       }
+
+      // email cleanup
+      return e
+        .replace(/[\[\(]dot[\]\)]/g, '.')
+        .replace(/[\[\(]at[\]\)]/g, '@')
+        .replace(/[\n\r]/g, ', ')
+        .replace(', , ', ', ');
     },
     response: makeResponse,
   },
