2a7b9abd7dfd129b3a5c8e53ea3e7905377134ea
Nemo <me@captnemo.in>
2017-12-12 00:41:48 +0530

    Adds a javascript bulld (for production)

---
+++ Gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+  gp_concat = require('gulp-concat'),
+  gp_rename = require('gulp-rename'),
+  gp_uglify = require('gulp-uglify');
+
+gulp.task('js', function() {
+  return gulp
+    .src([
+      'public/vue.js',
+      'public/vue-i18n.js',
+      'public/vue-resource.js',
+      'public/app.js'
+    ])
+    .pipe(gp_concat('app-single.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(gp_rename('app.min.js'))
+    .pipe(gp_uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['js']);
---
+++ _scripts/build.sh
@@ -0,0 +1,4 @@
+bundle install
+npm install
+./node_modules/.bin/gulp
+JEKYLL_ENV=production bundle exec jekyll build
